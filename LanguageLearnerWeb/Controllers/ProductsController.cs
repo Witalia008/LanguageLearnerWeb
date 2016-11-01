cd8567f779816246deb34808cc1823ad9e61b461
Witalia <kojvitalij@ukr.net>
2016-11-01 12:17:24 +0200

    Dull API

---
+++ LanguageLearnerWeb/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using LanguageLearnerWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LanguageLearnerWeb.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private Product[] products = new Product[]
+        {
+            new Product{ Id = 1, Name = "Product1", Category = "Cat1", Price=15.9m }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
