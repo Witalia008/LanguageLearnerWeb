c1303a9735e94a7b87d9cfe44b12934aa1412090
Witalia <Witalia@LONELYCODER>
2016-11-01 11:46:32 +0200

    Add project files.

---
+++ LanguageLearnerWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LanguageLearnerWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LanguageLearnerWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LanguageLearnerWeb.WebApiApplication" Language="C#" %>
