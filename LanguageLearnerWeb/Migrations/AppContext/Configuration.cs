26d34e2afb205f873a277cbc100a00301e2fbd6d
Witalia <kojvitalij@ukr.net>
2016-11-09 19:00:55 +0200

    Added Accounts & Authorisation for Web API

---
+++ LanguageLearnerWeb/Migrations/AppContext/Configuration.cs
@@ -0,0 +1,32 @@
+namespace LanguageLearnerWeb.Migrations.AppContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LanguageLearnerWeb.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\AppContext";
+        }
+
+        protected override void Seed(LanguageLearnerWeb.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
