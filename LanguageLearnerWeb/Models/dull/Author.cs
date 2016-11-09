3540c50fa75d821e51c573187e9f4a0d7c83045b
Witalia <kojvitalij@ukr.net>
2016-11-09 19:55:42 +0200

    Replaced dull API with DB related.

--- LanguageLearnerWeb/Models/dull/Author.cs
+++ LanguageLearnerWeb/Models/dull/Author.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace LanguageLearnerWeb.Models
+namespace LanguageLearnerWeb.Models.dull
 {
-    public class Product
+    public class Author
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string Category { get; set; }
-        public decimal Price { get; set; }
     }
 }
