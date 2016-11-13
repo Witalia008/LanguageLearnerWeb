684ff76341df4a6fce77f2e024d2be2d97fe9ecb
Witalia <kojvitalij@ukr.net>
2016-11-13 17:59:14 +0200

    Added Levels.

---
+++ LanguageLearnerWeb/Migrations/LLContext/201611131556381_AddLevels.cs
@@ -0,0 +1,27 @@
+namespace LanguageLearnerWeb.Migrations.LLContext
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddLevels : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Levels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        PointsRequired = c.Int(nullable: false),
+                        Title = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Levels");
+        }
+    }
+}
---
+++ LanguageLearnerWeb/Models/Public/Level.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LanguageLearnerWeb.Models
+{
+    public class Level
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int PointsRequired { get; set; }
+        [Required]
+        public string Title { get; set; }
+    }
+}
