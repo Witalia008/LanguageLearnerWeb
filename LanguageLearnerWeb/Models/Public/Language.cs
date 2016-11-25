af6ceab0c98706674ce4af583780fdf9b1cb8e22
Andrii Kalinichenko <Andrii Kalinichenko>
2016-11-25 14:25:47 +0200

    Added public model Language.

---
+++ LanguageLearnerWeb/Models/Public/Language.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LanguageLearnerWeb.Models
+{
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Source { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string NameOriginal { get; set; }
+        [Required]
+        public string ShortName { get; set; }
+        [RegularExpression(@"^[A-Z]{2}-[A-Z]{2}$")]
+        [StringLength(5)]
+        [Index(IsUnique = true)]
+        public string ShortNameCC { get; set; }
+    }
+}
