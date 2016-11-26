60681f71dac14357c01f2dd6426778c82577cff3
Andrii Kalinichenko <Andrii Kalinichenko>
2016-11-26 14:44:06 +0200

    Added public part of Preposition.

---
+++ LanguageLearnerWeb/Models/Public/Preposition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LanguageLearnerWeb.Models
+{
+    public class Preposition
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Language")]
+        public int LanguageId { get; set; }
+        public virtual Language Language { get; set; }
+
+        [Required]
+        public string Prefix { get; set; }
+        [Required]
+        public string Suffix { get; set; }
+        [Required]
+        public string Answer { get; set; }
+        [Required]
+        public string Options { get; set; }
+        public string Rule { get; set; }
+    }
+}
